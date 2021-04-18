531496c03efe68f313538268a923fef2ecf44cfc
7wumsH9O <caoy851@gmail.com>
2021-04-18 20:55:01 +0800

    Delete ikswap.sol

--- contracts/interface/ikswap.sol
+++
@@ -1,5 +0,0 @@
-pragma experimental ABIEncoderV2;
-interface IKswap {
-    function deposit(uint256 _pid, uint256 _amount) external ;
-    function withdraw(uint256 _pid, uint256 _amount) external ;
-}
