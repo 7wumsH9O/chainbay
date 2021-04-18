bfaca6ab22823373e4cd942a6e9e14b5dd25a905
7wumsH9O <caoy851@gmail.com>
2021-04-18 21:08:56 +0800

    Delete icake.sol

--- contracts/interface/icake.sol
+++
@@ -1,5 +0,0 @@
-pragma experimental ABIEncoderV2;
-interface ICake {
-    function deposit(uint256 _pid, uint256 _amount) external ;
-    function withdraw(uint256 _pid, uint256 _amount) external ;
-}
