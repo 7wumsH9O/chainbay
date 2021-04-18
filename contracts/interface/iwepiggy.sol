8bf0a53f72db5b2bb23df9128d42f0c8b0997df9
7wumsH9O <caoy851@gmail.com>
2021-04-18 21:06:08 +0800

    Delete iwepiggy.sol

--- contracts/interface/iwepiggy.sol
+++
@@ -1,8 +0,0 @@
-// https://wepiggy.com/
-interface IWepiggy {
-    function mint(uint256 _amount) external;
-    function redeem(uint256 redeemTokens) external ;
-    function claim() external ;
-    function balanceOfUnderlying(address _account) external view returns(uint256);
-    function balanceOf(address _account) external view returns(uint256);
-}
