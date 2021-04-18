b77e8bcf46be3260b39d76365f25cd2bc66cff5c
7wumsH9O <caoy851@gmail.com>
2021-04-18 21:09:02 +0800

    Delete ilhb.sol

--- contracts/interface/ilhb.sol
+++
@@ -1,6 +0,0 @@
-interface ILHB {
-    function mint(uint256 _amount) external;
-    function redeem(uint256 _amount) external ;
-    function balanceOf(address _account) external view returns(uint256);
-    function claimComp(address holder, address[] memory cTokens) external;
-}
