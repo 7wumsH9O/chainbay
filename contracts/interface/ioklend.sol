d0f1eae837c84c57bacbfdc1cee1c1b57a4ca1ed
7wumsH9O <caoy851@gmail.com>
2021-04-18 21:08:35 +0800

    Delete ioklend.sol

--- contracts/interface/ioklend.sol
+++
@@ -1,5 +0,0 @@
-interface IOKLend {
-    function mint(uint256 _amount) external;
-    function redeem(uint256 _amount) external ;
-    function balanceOf(address _account) external view returns(uint256);
-}
